73154332c8280f4940a490c4f55e90fcb22d9709
khekalea <[khekale@gmail.com]>
2025-07-22 22:55:35 +0530

    Assignment Ones add

---
+++ Assig1-Q1.py
@@ -0,0 +1,5 @@
+print('Welcome to Python programming')
+print('The Addition of 5 and 4 is :',4+5) # Simple Calculator
+print('I like coading')
+print("The Multiplication is",4*5) # Simple Calculator
+print('Have a Good day!!!')
---
+++ Assig1-Q2.py
@@ -0,0 +1,6 @@
+print("This is Python")
+def detail(name,age): # By pasiing parameters
+    print("My name is:",name)
+    print("And age is:",age)
+print("Here are the details")
+detail("Aditi",24) # Function calling
