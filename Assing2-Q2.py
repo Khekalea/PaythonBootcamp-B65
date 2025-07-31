3be9715797dc0db7e713ed14cd3da247f1940929
khekalea <[khekale@gmail.com]>
2025-07-31 18:33:11 +0530

    variable type print

---
+++ Assing2-Q2.py
@@ -0,0 +1,26 @@
+a=10
+b="Python"
+c=3.14
+name="Jhon"
+fruits=["Apple","Banana","Orange"]
+coordinates=(10.5,20.3)
+user={"Name":"Jhon Doe","Age":25}
+x = {"apple", "banana", "cherry"}
+y = frozenset({"apple", "banana", "cherry"})
+z = True
+x1 = None
+y1 = b"Hello"
+
+
+print(type(a),a)
+print(type(b),b)
+print(type(c),c)
+print(type(name),name)
+print(type(fruits),fruits)
+print(type(coordinates),coordinates)
+print(type(user),user)
+print(type(x),x)
+print(type(y),y)
+print(type(z),z)
+print(type(x1),x1)
+print(type(y1),y1)
